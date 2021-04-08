9e006f61ce290911228db187e0c6bc2ed1117c65
jorge arguelles <jorgeariasarguelles@gmail.com>
2021-04-08 18:06:17 -0500

    add/[id].js page

--- src/pages/Country/[id].js
+++ src/pages/Country/[id].js
@@ -1,5 +1,44 @@
-const Country = () => {
-	return <div>Country</div>;
+import Layout from "../../components/Layout/Layout";
+import styles from "./country.module.css";
+
+const Country = ({ country }) => {
+	return (
+		<Layout title={country.name}>
+			<div>
+				<div className={styles.overview_panel}>
+					<img src={country.flag} alt={country.name} />
+
+					<h1 className={styles.overview_name}>{country.name}</h1>
+					<div className={styles.overview_region}>{country.region}</div>
+
+					<div className={styles.overview_numbers}>
+						<div className={styles.overview_population}>
+							<div className={styles.overview_value}> {country.population}</div>
+							<div className={styles.overview_label}> Población:</div>
+						</div>
+						<div className={styles.overview_area}>
+							<div className={styles.overview_value}> {country.area}</div>
+							<div className={styles.overview_label}> Aréa:</div>
+						</div>
+					</div>
+				</div>
+
+				<div className={styles.details_panel}> </div>
+			</div>
+		</Layout>
+	);
 };
 
 export default Country;
+
+export const getServerSideProps = async ({ params }) => {
+	const res = await fetch(
+		`https://restcountries.eu/rest/v2/alpha/${params.id}`
+	);
+
+	const country = await res.json();
+
+	return {
+		props: { country },
+	};
+};
