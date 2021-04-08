c18a2fd26a20cd2b02ff79773d1cef6aacd181a7
jorge arguelles <jorgeariasarguelles@gmail.com>
2021-04-08 15:47:15 -0500

    update/ Countires table componenet with filters is done

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,15 +2,33 @@ import styles from "../styles/Home.module.css";
 import Layout from "../components/Layout/Layout";
 import SearchInput from "../components/SearchInput/SearchInput";
 import CountriesTable from "../components/CountriesTable/CountriesTable";
+import { useState } from "react";
 
 export default function Home({ countries }) {
+	const [keyword, setKeyword] = useState("");
+
+	const filteredCountries = countries.filter(
+		(country) =>
+			country.name.toLowerCase().includes(keyword) ||
+			country.region.toLowerCase().includes(keyword) ||
+			country.subregion.toLowerCase().includes(keyword)
+	);
+
+	const onInputChange = (e) => {
+		e.preventDefault();
+		setKeyword(e.target.value.toLowerCase());
+	};
+
 	return (
 		<Layout>
 			<div className={styles.counts}>
 				Se encontraron {countries.length} paises
 			</div>
-			<SearchInput placeholder="Filtrar por Nombre, Región o Sub Región" />
-			<CountriesTable countries={countries} />
+			<SearchInput
+				placeholder="Filtrar por Nombre, Región o Sub Región"
+				onChange={onInputChange}
+			/>
+			<CountriesTable countries={filteredCountries} />
 		</Layout>
 	);
 }
