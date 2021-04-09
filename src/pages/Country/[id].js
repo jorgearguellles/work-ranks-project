7b061e4580f030887611b32556838c8850ddc517
jorge arguelles <jorgeariasarguelles@gmail.com>
2021-04-09 10:45:05 -0500

    update/[id].js page details about borders countries

--- src/pages/Country/[id].js
+++ src/pages/Country/[id].js
@@ -37,17 +37,17 @@ const Country = ({ country }) => {
 					<div className={styles.overview_numbers}>
 						<div className={styles.overview_population}>
 							<div className={styles.overview_value}> {country.population}</div>
-							<div className={styles.overview_label}> Población:</div>
+							<div className={styles.overview_label}> Population</div>
 						</div>
 						<div className={styles.overview_area}>
 							<div className={styles.overview_value}> {country.area}</div>
-							<div className={styles.overview_label}> Aréa:</div>
+							<div className={styles.overview_label}> Area</div>
 						</div>
 					</div>
 				</div>
 
 				<div className={styles.details_panel}>
-					<h4 className={styles.details_panel_heading}>Detalles</h4>
+					<h4 className={styles.details_panel_heading}>Details</h4>
 
 					<div className={styles.details_panel_row}>
 						<div className={styles.details_panel_label}>Capital</div>
@@ -55,21 +55,21 @@ const Country = ({ country }) => {
 					</div>
 
 					<div className={styles.details_panel_row}>
-						<div className={styles.details_panel_label}>Lengua / Idioma</div>
+						<div className={styles.details_panel_label}>Languages</div>
 						<div className={styles.details_panel_value}>
 							{country.languages.map(({ name }) => name).join(", ")}
 						</div>
 					</div>
 
 					<div className={styles.details_panel_row}>
-						<div className={styles.details_panel_label}>Moneda local</div>
+						<div className={styles.details_panel_label}>Currency</div>
 						<div className={styles.details_panel_value}>
 							{country.currencies.map(({ name }) => name).join(", ")}
 						</div>
 					</div>
 
 					<div className={styles.details_panel_row}>
-						<div className={styles.details_panel_label}>Nombre nativo</div>
+						<div className={styles.details_panel_label}>Native Name</div>
 						<div className={styles.details_panel_value}>
 							{country.nativeName}
 						</div>
@@ -81,12 +81,19 @@ const Country = ({ country }) => {
 					</div>
 
 					<div className={styles.details_panel_borders}>
-						{borders.map(({ flag, name }) => {
-							<div className={styles.details_panel_borders_country}>
-								<img src={flag} alt={name} />
-								<div className={styles.details_panel_borders_name}>{name}</div>
-							</div>;
-						})}
+						<div className={styles.details_panel_borders_label}>
+							Paises vecinos
+						</div>
+						<div className={styles.details_panel_borders_container}>
+							{borders.map(({ flag, name }) => {
+								<div className={styles.details_panel_borders_country}>
+									<img src={flag} alt={name} />
+									<div className={styles.details_panel_borders_name}>
+										{name}
+									</div>
+								</div>;
+							})}
+						</div>
 					</div>
 				</div>
 			</div>
