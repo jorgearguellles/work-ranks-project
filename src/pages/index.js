271dcfae0e87abb4ca32e41148612117b12cb274
jorge arguelles <jorgeariasarguelles@gmail.com>
2021-04-11 18:42:07 -0500

    update/ testing agsin

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,13 @@ import { useState } from "react";
 export default function Home({ countries }) {
 	const [keyword, setKeyword] = useState("");
 
+	const filteredCountries = countries.filter(
+		(country) =>
+			country.name.toLowerCase().includes(keyword) ||
+			country.region.toLowerCase().includes(keyword) ||
+			country.subregion.toLowerCase().includes(keyword)
+	);
+
 	const onInputChange = (e) => {
 		e.preventDefault();
 		setKeyword(e.target.value.toLowerCase());
